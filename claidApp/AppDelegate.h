9777133153a0e133d43948fc3e9c82fd73a68843
kevinpc <kevin@kevinpcdeMac-mini.local>
2016-10-18 10:13:05 +0800

    Initial Commit

---
+++ claidApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  claidApp
+//
+//  Created by kevinpc on 2016/10/18.
+//  Copyright © 2016年 kevinpc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ claidApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  claidApp
+//
+//  Created by kevinpc on 2016/10/18.
+//  Copyright © 2016年 kevinpc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
